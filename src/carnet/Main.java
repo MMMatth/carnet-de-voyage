a44503c1edef588ec522ef8939c2c9de4cbdb1f8
MMMatth <mat.loup04@gmail.com>
2024-05-11 16:08:09 +0200

    Ajout de la première Vue pour la page accueil prochaine étape contrôleur etc

--- src/carnet/Main.java
+++ src/carnet/Main.java
@@ -1,4 +1,24 @@
 package carnet;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import carnet.model.Carnet;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Carnet carnet = new Carnet();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/PageAccueil.fxml"));        stage.setTitle("Carnet de notes");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+
+    }
 }
